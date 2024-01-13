284db3876675bccc752f48b641f2a17c3b588a2d
a-pacifique <132818093+a-pacifique@users.noreply.github.com>
2024-01-13 12:07:22 +0200

    Update settings.py

--- generator/settings.py
+++ generator/settings.py
@@ -113,6 +113,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '13.51.109.173', '172.31.43.133', 'theflippereconomics.online']
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
