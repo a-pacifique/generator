1128c5a22dc13488e5967fddbeb770f5b078cf2b
a-pacifique <132818093+a-pacifique@users.noreply.github.com>
2024-02-08 21:35:54 +0200

    Update urls.py

--- Articles/urls.py
+++ Articles/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('create_article/', views.create_article, name='create_article'),
     path('about-us/', views.about_us, name='about_us'),
     path('contact-us/', views.contact_us, name='contact_us'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
     path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('ads.txt', views.ads_txt, name='ads_txt'),
